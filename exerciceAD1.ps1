0532a991c78c3b935405cfd4feec85f07a3b8c97
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-04-03 10:36:35 +0200

    Version finale

---
+++ exerciceAD1.ps1
@@ -0,0 +1,16 @@
+﻿#========================================#
+#                           	         #
+# NOM : créerUO                          #
+# AUTEUR : TERPREAU Maximilien           #
+# DATE : 03 avril 2024 à 11h15           #
+#                           	         #
+#========================================#
+
+<# Création d'une Unité d'Organisation #>
+$réponse = Read-Host "Voulez vous créer une Unité d'Organisation (Y/n) ?"
+
+Do{
+$nomUO = Read-Host "Quel sera son nom ?"
+New-ADOrganizationalUnit -Name $nomUO -Path "DC=local,DC=edimbourg,DC=cub,DC=sioplc,DC=fr"
+$réponse = Read-Host "Voulez- vous créer une autre UO (Y/n) ?"
+} While ($réponse -eq 'Y' )
