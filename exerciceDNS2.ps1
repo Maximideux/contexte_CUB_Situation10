c6ef0dca7e3164bc98b058c3f59148b40254b915
unknown <administrateur@local.edimbourg.cub.sioplc.fr>
2024-04-03 09:56:08 +0200

    Version finale

--- exerciceDNS2.ps1
+++ exerciceDNS2.ps1
@@ -1,21 +1,21 @@
-﻿#========================================#
-#                           	         #
-# NOM : créerEnregistrementDNSTypeA      #
-# AUTEUR : TERPREAU Maximilien           #
-# DATE : 03 avril 2024 à 11h15           #
-#                           	         #
-#========================================#
+﻿#============================================#
+#                           	             #
+# NOM : créerEnregistrementDNSTypeAOuCNAME   #
+# AUTEUR : TERPREAU Maximilien               #
+# DATE : 03 avril 2024 à 11h15               #
+#                           	             #
+#============================================#
 
 <# Création d'un enregistrement DNS de Type A (adresse IP et CM) ou CNAME#>
-[string]$typeEnregistrement = Read-Host "Quel type d'enregistrement voulez-vous ? (A ou CNAME) ?"
+Add-Type -AssemblyName Microsoft.VisualBasic
+[string]$typeEnregistrement = [Microsoft.VisualBasic.Interaction]::InputBox("Quel type d'enregistrement voulez-vous ? (A ou CNAME) ?")
 if ($typeEnregistrement -eq "A") {
-    $nombreHotes = Read-Host "Combien d'hôtes de type A souhaitez-vous enregistrer ?"
+    $nombreHotes = [Microsoft.VisualBasic.Interaction]::InputBox("Combien d'hôtes de type A souhaitez-vous enregistrer ?")
     $informationsA = @()
     for ($i = 1; $i -le $nombreHotes; $i++) {
-        Write-Host "Hôte numéro $i"
-        $nomHote = Read-Host "Quel est le nom de l'hôte ?"
-        $zoneHote = Read-Host "Quelle est la zone (local.edimbourg.cub.sioplc.fr) ?"
-        $adresseReseau = Read-Host "Quelle est l'adresse réseau associée à l'hôte $nomHote ?"
+        $nomHote = [Microsoft.VisualBasic.Interaction]::InputBox("Quel est le nom de l'hôte ?", "Hôte numéro $i")
+        $zoneHote = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est la zone (local.edimbourg.cub.sioplc.fr) ?", "Hôte numéro $i")
+        $adresseReseau = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est l'adresse réseau associée à l'hôte $nomHote ?", "Hôte numéro $i")
         $informationsA += [PSCustomObject]@{
             Type = $typeEnregistrement
             NomHote = $nomHote
@@ -24,22 +24,27 @@ if ($typeEnregistrement -eq "A") {
         }
         try {
             $enregistrement = Add-DnsServerResourceRecordA -Name $nomHote -ZoneName $zoneHote -IPv4Address $adresseReseau
-            Write-Host "Informations sur les hôtes enregistrés :"
-            $informationsHotes
+            [Microsoft.VisualBasic.Interaction]::MsgBox("Informations sur les hôtes enregistrés :" , "Information" , "Enregistrements")
+            $resultat = Get-DnsServerResourceRecord -ZoneName $zoneHote
+            $message = ""
+                foreach ($prop in $resultat | Get-Member -MemberType Properties) 
+                {
+                    $message += "$($prop.Name): $($resultat.$($prop.Name))`n"
+                }
+            [Microsoft.VisualBasic.Interaction]::MsgBox($message, "Information", "Informations des enregistrements")
         }
         catch {
-            Write-Host "Il y a eu une erreur dans le code, les informations rentrées ne sont pas correctes"
+            [Microsoft.VisualBasic.Interaction]::MsgBox("Il y a eu une erreur dans le code, les informations rentrées ne sont pas correctes")
         }
         $informationsA = $null
     }
 }elseif ($typeEnregistrement -eq "CNAME") {
-    $nombreHotes = Read-Host "Combien d'hôtes de type CNAME souhaitez-vous enregistrer ?"
+    $nombreHotes = [Microsoft.VisualBasic.Interaction]::InputBox("Combien d'hôtes de type CNAME souhaitez-vous enregistrer ?")
     $informationsCNAME = @()
     for ($i = 1; $i -le $nombreHotes; $i++) {
-        Write-Host "Hôte numéro $i"
-        $nomHote = Read-Host "Quel est le nom de l'hôte ?"
-        $zoneHote = Read-Host "Quelle est la zone (local.edimbourg.cub.sioplc.fr) ?"
-        $nomAlias = Read-Host "Quelle est le nom alias associé à $nomHote ?"
+        $nomHote = [Microsoft.VisualBasic.Interaction]::InputBox("Quel est le nom de l'hôte ?", "Hôte numéro $i")
+        $zoneHote = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est la zone (local.edimbourg.cub.sioplc.fr) ?", "Hôte numéro $i")
+        $nomAlias = [Microsoft.VisualBasic.Interaction]::InputBox("Quelle est le nom alias associé à $nomHote ?", "Hôte numéro $i")
         $informationsCNAME += [PSCustomObject]@{
             Type = $typeEnregistrement
             NomHote = $nomHote
@@ -48,14 +53,20 @@ if ($typeEnregistrement -eq "A") {
         }
         try {
             Add-DnsServerResourceRecordCName -Name $nomHote -HostNameAlias $nomAlias -ZoneName $zoneHote
-            Write-Host "Informations sur les hôtes enregistrés :"
-            $informationsCNAME
-        }
+            [Microsoft.VisualBasic.Interaction]::MsgBox("Informations sur les hôtes enregistrés :" , "Information" , "Enregistrements")
+            $message = ""
+            $resultat = Get-DnsServerResourceRecord -ZoneName $zoneHote
+            foreach ($prop in $resultat | Get-Member -MemberType Properties) 
+                {
+                    $message += "$($prop.Name): $($resultat.$($prop.Name))`n"
+                }
+            [Microsoft.VisualBasic.Interaction]::MsgBox($message, "Information", "Informations des enregistrements")
+            }
         catch {
-            Write-Host "Il y a eu une erreur dans le code, les informations rentrées ne sont pas correctes"
-        }
+            [Microsoft.VisualBasic.Interaction]::MsgBox("Il y a eu une erreur dans le code, les informations rentrées ne sont pas correctes")
+            }
         $informationsCNAME = $null
     }
 }else{
-    Write-Host "Ce n'est pas un type d'enregistrement DNS!"
+    [Microsoft.VisualBasic.Interaction]::MsgBox("Ce n'est pas un type d'enregistrement DNS!" , "Informations" , "Erreur de type !")
 }
